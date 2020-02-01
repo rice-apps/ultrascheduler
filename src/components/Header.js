7e11e8c52686f3563d83a9864179eb32a2fcc611
Max Bowman <maxabowman@gmail.com>
2020-02-01 13:36:04 -0600

    Minor updates.

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Title from "Title";
+
+function Header() {
+    return (
+        <Title></Title>
+    );
+}
+
+export default Header;
---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Title() {
+    return (
+        <div>
+            <h1>Ultrascheduler</h1>
+            <h3>A Course Planning Tool</h3>
+        </div>
+    );
+}
+
+export default Title;
